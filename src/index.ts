a235f712272c55e8d6fe93429d7c03e0bd6ba280
Savio (Lamarti) <90478550+saviops10@users.noreply.github.com>
2022-02-07 20:52:30 -0300

    Desafio02_NodeJS
    Concluido

--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,75 @@
-/**
- * Required External Modules
- */
 
-import * as dotenv from "dotenv";
-import express from "express";
-import cors from "cors";
-import helmet from "helmet";
+console.log('Desafio 02 - NodeJS')
 
-dotenv.config();
+const nome1:string = 'Ubuntu1 Silva';
+const idade1:number = 33;
+const peso1:number = 88.5;
+const altura1:number = 1.72;
+const ehDev1:boolean = true;
+const imc1:number = peso1 /(altura1*altura1);
 
-/**
- * App Variables
- */
+const nome2:string = 'Ubuntu2 Souza';
+const idade2:number = 31;
+const peso2:number = 82.5;
+const altura2:number = 1.65;
+const ehDev2:boolean = true;
+const imc2:number = peso2 / (altura2+altura2);
 
-if (!process.env.PORT) {
-	process.exit(1);
+const nome3:string = 'Ubuntu3 Silva';
+const idade3:number = 28;
+const peso3:number = 78.3;
+const altura3:number = 1.82;
+const ehDev3:boolean = true;
+const imc3:number = peso3 / (altura3+altura3);
+
+//R01 - Somar idade Ubuntus
+const soma:number = idade1 + idade2 + idade3;
+//R02 - Agrupar nome dos Ubuntus
+console.log(nome1+' '+nome2+' '+nome3);
+//R03 - Calcular média IMC
+const imcMedia:number = (imc1+imc2+imc3) / 3; 
+console.log('Média de IMCs: '+imcMedia);
+//R04 - Quantos ubuntus São Devs
+var ehDev:any = new Boolean(0);
+
+if (ehDev1 == true){
+	ehDev++;
+}
+else {
+	ehDev = ehDev;
 }
 
-const PORT: number = parseInt(process.env.PORT as string, 10);
+if (ehDev2 == true){
+	ehDev++;
+}
+else {
+	ehDev=ehDev;
+}
+if (ehDev3 == true){
+	ehDev++;
+}
+else {
+	ehDev=ehDev;
+};
+console.log(`Temos ${ehDev}, entre Ubuntus que são Devs!`);
 
-const app = express();
+//R05 - Quais Ubuntus tem Silva no nome
+var Silva1:boolean = nome1.includes('Silva');
+var Silva2:boolean = nome2.includes('Silva');
+var Silva3:boolean = nome3.includes('Silva');
+var Silva:string = '';
 
-/**
- *  App Configuration
- */
+if (Silva1 == true){
+	Silva = Silva + ` ${nome1}`;
+}
+if (Silva2 == true){
+	Silva = Silva + ` e ${nome2}`;
+}
+if (Silva3 == true){
+	Silva = Silva + ` e ${nome3}`;
+}
 
-app.use(helmet());
-app.use(cors());
-app.use(express.json());
+console.log(`Dentre os Ubuntus: ${Silva}, possuem Silva no Nome.`);
 
-/**
- * Server Activation
- */
-
-app.listen(PORT, () => {
-	console.log(`Listening on port ${PORT}`);
-
-
-	// CÓDIGO PARA ATENDER OS REQUERIMENTOS
-	// R01, R02, R03, R04, R05
-	
-});
+//FIM do codigo
+console.log('Desafio 02 NojeJS Concluído...');
